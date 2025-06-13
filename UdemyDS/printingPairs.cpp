20680494b235bcb245d11c1b1f31bd06f0596c39
Shikha D <shikhadekate171@gmail.com>
2025-06-13 17:00:26 +0000

    DS problems: Udemy DS
    Array problems

---
+++ UdemyDS/printingPairs.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    // int arr[] = {1, 2, 3, 4, 5};
+    vector<int> arr {1, 2, 3, 4, 5};
+
+    for(auto& i : arr){
+        cout<< i << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
